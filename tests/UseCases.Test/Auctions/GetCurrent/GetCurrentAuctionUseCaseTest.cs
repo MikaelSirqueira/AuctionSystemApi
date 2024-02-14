fac7a5b8a4d4cbba82e9f2c6bd8e274a3276ecda
Mikael Sirqueira <silmike14@yahoo.com.br>
2024-02-14 15:07:43 -0300

    :test_tube: test: add tests

---
+++ tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,53 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Enums;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+using Xunit;
+
+namespace UseCases.Test.Auctions.GetCurrent;
+public class GetCurrentAuctionUseCaseTest
+{
+    [Fact]
+    public void Success()
+    {
+        // ARRANGE - instancia do que precisa        
+
+        // o 'f' é de fake
+        var entity = new Faker<Auction>()
+            .RuleFor(auction => auction.Id, f => f.Random.Number(1, 700))
+            .RuleFor(auction => auction.Name, f => f.Lorem.Word())
+            .RuleFor(auction => auction.Starts, f => f.Date.Past())
+            .RuleFor(auction => auction.Ends, f => f.Date.Future())
+            .RuleFor(auction => auction.Items, (f, auction) => new List<Item>
+            { 
+                new Item 
+                {
+                    Id = 1,
+                    Name = f.Commerce.ProductName(),
+                    Brand = f.Commerce.Department(),
+                    BasePrice = f.Random.Decimal(50, 1000),
+                    Condition = f.PickRandom<Condition>(),
+                    AuctionId = auction.Id
+                }
+            }).Generate();
+
+
+        var mock = new Mock<IAuctionRepository>();
+        // Necessário fazer isso para o mock entender que precisa instanciar um leilao
+        mock.Setup(i => i.GetCurrent()).Returns(entity);
+
+        var useCase = new GetCurrentAuctionUseCase(mock.Object);
+
+        // ACT - Executa o que precisa
+        var auction = useCase.Execute();
+
+        // ASSERT - Verifica o resultado
+        auction.Should().NotBeNull();
+        auction.Id.Should().Be(entity.Id);
+        auction.Name.Should().Be(entity.Name);
+    }
+}
---
+++ tests/UseCases.Test/Offers/CreateOffer/CreateOfferUseCaseTest.cs
@@ -0,0 +1,44 @@
+﻿using Bogus;
+using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.Communication.Requests;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Services;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UseCases.Test.Offers.CreateOffer;
+public class CreateOfferUseCaseTest
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Success(int itemId)
+    {
+        // ARRANGE - instancia do que precisa        
+        var request = new Faker<RequestCreateOfferJson>()
+            .RuleFor(request => request.Price, f => f.Random.Decimal(1, 700)).Generate();
+
+
+        var offerRepository = new Mock<IOfferRepository>();        
+        var loggedUser = new Mock<ILoggedUser>();
+        loggedUser.Setup(i => i.User()).Returns(new User());
+
+        var useCase = new CreateOfferUseCase(loggedUser.Object, offerRepository.Object);
+
+        // ACT - Executa o que precisa
+        var act = () => useCase.Execute(itemId, request);
+
+        // ASSERT - Verifica o resultado
+        act.Should().NotThrow();
+
+    }
+}
